08ee0947d9b6ca07973b8468c73ace6525bce96a
alrifatsabbir <alrifatsabbir@gmail.com>
2026-02-04 13:37:40 +0600

    release-v.1.0.1(Speed Insights implement for vercel UPDATE#20)

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import App from "./App.tsx";
 import "./index.css";
 
@@ -12,6 +13,7 @@ if (rootElement) {
       <div className="min-h-screen w-full bg-white dark:bg-zinc-950 selection:bg-indigo-500/30">
         <App />
         <Analytics />
+        <SpeedInsights/>
       </div>
     </React.StrictMode>
   );
